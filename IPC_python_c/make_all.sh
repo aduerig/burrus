0d3f2bccb7b9dc9cc8f55ba104f888d3e579f490
Andrew <duerig.andrew@gmail.com>
2018-04-21 18:35:55 -0400

    Removed old folder

--- IPC_python_c/make_all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Linker opts should be blank for OS X, FreeBSD and OpenSolaris
-#LINKER_OPTIONS=""
-
-# On Linux, we must link with realtime and thread libraries
-LINKER_OPTIONS="-lrt -lpthread"
-
-g++ -Wall -std=c++11 -o new_premise new_premise.cpp -L. $LINKER_OPTIONS
