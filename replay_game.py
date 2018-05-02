4c62bedbe582f222f2352fc0ef11584c0ee1b2b1
Andrew <duerig.andrew@gmail.com>
2018-05-02 15:20:49 -0400

    GUI is functional

--- replay_game.py
+++ replay_game.py
@@ -1,6 +1,109 @@
 import numpy as np
 import os
 import utils
+import time
+
+from tkinter import *
+from tkinter import messagebox
+
+
+def draw_lines(canvas, width, height):
+    holder = []
+
+    # vertical lines
+    curr_x = 0
+    inc_x = width / 8
+    for i in range(8):
+        line = canvas.create_line(curr_x, 0, curr_x, height, fill = 'black')
+        holder.append(line)
+        curr_x += inc_x
+
+    # horizontal lines
+    curr_y = 0
+    inc_y = height / 8
+    for i in range(8):
+        line = canvas.create_line(0, curr_y, width, curr_y, fill = 'black')
+        holder.append(line)
+        curr_y += inc_y
+    return holder
+
+
+def draw_circle(canvas, width, height, pos):
+    if(pos >= 64):
+        color = "white"
+        pos = pos - 64
+    else:
+        color = "black"
+
+    x = pos % 8
+    y = int(pos / 8)
+    
+    offset = 5
+    square_x_size = (width / 8)
+    square_y_size = (height / 8)
+    # circle_x_size = (width / 8) - offset
+    # circle_y_size = (height / 8) - offset
+    
+    circ = canvas.create_oval(x * square_x_size + offset, y * square_y_size + offset, 
+                            (x+1) * square_x_size - offset, 
+                            (y+1) * square_y_size - offset, 
+                            fill = color)
+    return circ
+
+
+def click_callback(event, canvas, game_data, counter, objs, width, height):
+    for obj in objs:
+        canvas.delete(obj)
+    objs.clear()
+
+    temp = 0
+    # utils.print_board(game_data[0][counter[0]])
+    for circle in game_data[0][counter[0]]:
+        # print(game_data[0][counter[0]])
+        if(circle == 1):
+            objs.append(draw_circle(canvas, width, height, temp))
+        temp += 1
+
+    counter[0] += 1
+    if(counter[0] == len(game_data[0])):
+        print("reached end of games, exiting")
+        exit(0)
+
+
+def close(event):
+    exit() # if you want to exit the entire thing
+
+
+def display_game(games):
+    top = Tk()
+
+    w = 500
+    h = 500
+    bg_color = "salmon4"
+    canvas = Canvas(top, bg = bg_color, height = h, width = w)
+
+    text = Label(top, text="Rouge", height=2, bg = bg_color)
+    text.pack()
+
+    # coord = [10, 50, 240, 210]
+    # arc = canvas.create_arc(coord, start = 0, extent = 150, fill = "red")
+    # line = canvas.create_line(10, 10, 200, 200, fill = 'white')
+
+    lines = draw_lines(canvas, w, h)
+
+    counter = [0]
+    objs = []
+    click_callback(None, canvas, games, counter, objs, w, h)
+    canvas.focus_set()
+
+    canvas.bind("<Button-1>", lambda x: click_callback(x, canvas, games, counter, objs, w, h))
+    canvas.bind("<space>", lambda x: click_callback(x, canvas, games, counter, objs, w, h))
+    canvas.bind("<Return>", lambda x: click_callback(x, canvas, games, counter, objs, w, h))
+    canvas.bind("<Escape>", close)
+
+    canvas.pack()
+    top.mainloop()
+
 
 
 def load_games(games_dir):
@@ -14,9 +117,13 @@ def main():
     print('Using model at: ' + data_dir)
     games = load_games(data_dir)
 
-    for i in reversed(range(len(games[0]))):
-        utils.print_board(games[0][i])
-        input("Press enter to see next board state")
+    # for i in reversed(range(len(games[0]))):
+    #     utils.print_board(games[0][i])
+    #     input("Press enter to see next board state")
+
+    inv_games = [list(reversed(games[0])), list(reversed(games[1])), list(reversed(games[2]))]
+    display_game(inv_games)
+
 
 if __name__ == "__main__":
     main()
