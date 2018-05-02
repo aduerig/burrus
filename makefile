a02dccf6c06512b0dcbdd0f03780e0c81d137921
Andrew <duerig.andrew@gmail.com>
2018-05-02 03:15:19 -0400

    Refractoring

--- makefile
+++ makefile
@@ -5,15 +5,15 @@ LINKER_OPTIONS= -lrt -lpthread
 
 
 DRIVER_HEADERS = communicator.hpp engine.hpp player.hpp
-PLAY_HEADERS = engine.hpp player.hpp
-PARAM_HEADERS = engine.hpp player.hpp param_serial.hpp
+PLAY_HEADERS = communicator.hpp engine.hpp player.hpp
+RECORD_HEADERS = communicator.hpp engine.hpp player.hpp
 
-DRIVER_OBJECTS = communicator.cpp engine.cpp driver.cpp player.cpp
-PLAY_OBJECTS = play.cpp engine.cpp player.cpp
-PARAM_OBJECTS = engine.cpp player.cpp param_serial.cpp
+DRIVER_OBJECTS = communicator.cpp engine.cpp driver.cpp player.cpp human.cpp random.cpp minimax.cpp montecarlo.cpp
+PLAY_OBJECTS = communicator.cpp play.cpp engine.cpp player.cpp human.cpp random.cpp minimax.cpp montecarlo.cpp
+RECORD_OBJECTS = communicator.cpp engine.cpp player.cpp human.cpp random.cpp minimax.cpp montecarlo.cpp record_games.cpp
 
 
-all: driver
+all: driver record_games
 
 driver: $(DRIVER_OBJECTS)
 	$(CC) $(CFLAGS) $(DRIVER_HEADERS) $(DRIVER_OBJECTS) -o driver $(LINKER_OPTIONS)
@@ -21,16 +21,12 @@ driver: $(DRIVER_OBJECTS)
 play: $(PLAY_OBJECTS)
 	$(CC) $(CFLAGS) $(PLAY_HEADERS) $(PLAY_OBJECTS) -o play $(LINKER_OPTIONS)
 
-param: $(PARAM_OBJECTS)
-	$(CC) $(CFLAGS) $(PARAM_HEADERS) $(PARAM_OBJECTS) -o param_serial $(LINKER_OPTIONS)
-
-himpi:
-	$(MPICC) $(CFLAGS) hi_mpi.cpp -o hi_mpi $(LINKER_OPTIONS)
+record_games: $(RECORD_OBJECTS)
+	$(CC) $(CFLAGS) $(RECORD_HEADERS) $(RECORD_OBJECTS) -o record_games $(LINKER_OPTIONS)
 
 clean:
 	-rm -f driver
 	-rm -f play
+	-rm -f record_games
 	-rm -f callgrind.out.*
 	-rm -f out.log
-	-rm -f tensor_driver
-	-rm -f param_serial
--- record_games.cpp
+++ record_games.cpp
@@ -21,7 +21,7 @@
 #include <vector>
 #include "engine.hpp"
 #include "player.hpp"
-#include "param_serial.hpp"
+#include "communicator.hpp"
 
 
 /*  
@@ -38,13 +38,7 @@
  */
 
 
-Params::Params()
-{
-    // blank
-}
-
-
-std::string Params::get_newest_model_name()
+std::string get_newest_model_name()
 {
     FILE *fp;
     char *fnm = (char *) calloc(50, sizeof(char));
@@ -94,7 +88,7 @@ std::string Params::get_newest_model_name()
  */
 
 
-void Params::load_ull_into_int_arr(Engine* e, int* int_board_loader, U64 from_board)
+void load_ull_into_int_arr(Engine* e, int* int_board_loader, U64 from_board)
 {
     int index;
     // printf("%llu\n", from_board);
@@ -109,8 +103,28 @@ void Params::load_ull_into_int_arr(Engine* e, int* int_board_loader, U64 from_bo
     }
 }
 
+// https://stackoverflow.com/questions/440133/how-do-i-create-a-random-alpha-numeric-string-in-c
+std::string gen_random(const int len) 
+{
+    std::string new_str;
+    for(int i = 0; i < len; i++)
+    {
+        new_str.append(" ");
+    }
 
-void Params::save_game_info(std::string model_path, int local_rank, int game_number, Engine* e, 
+    static const char alphanum[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) 
+    {
+        new_str[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    return new_str;
+}
+
+
+void save_game_info(std::string model_path, int local_rank, int game_number, Engine* e, 
                                         int num_moves, int result, float **MC_chances, float *saved_values, int* no_decision_arr, int* decisions_made)
 {
     // Filename: ${model_path}/games/${local_rank}_${game_number}.game
@@ -232,51 +246,52 @@ void Params::save_game_info(std::string model_path, int local_rank, int game_num
  * NOTE: the game files are written backwards (last move first), so I populate the arrays end to start, 
  *       so they are in the correct order
  */
-void Params::read_game_info(std::string game_path, int *n_moves, U64 *player_boards, U64 *opponent_boards, float **MCvals, int *result, float *saved_values)
-{
 
-    // dummy holder
-    char* dummy_return;
-    // Open file
-    FILE *fp = fopen(game_path.c_str(),"r");
-    // Read number of moves
-    char *buf = (char *) calloc(30, sizeof(char));
-    dummy_return = fgets(buf, 30, fp);
-    *n_moves = atoi(buf);
-    // Allocate memory
-    player_boards = (U64 *) calloc(n_moves[0], sizeof(U64)); 
-    opponent_boards = (U64 *) calloc(n_moves[0], sizeof(U64)); 
-    MCvals = (float **) calloc(n_moves[0], sizeof(float *));
-    result = (int *) calloc(n_moves[0], sizeof(int));
-    saved_values = (float *) calloc(n_moves[0], sizeof(float));
-    // Loop over number of moves
-    for (int i = n_moves[0]-1;i >= 0; --i)
-    {
-        // Read player board
-        dummy_return = fgets(buf,30,fp);
-        // Turn a string rep of U64 into a U64 rep and store in player_boards[i]
-        sscanf(buf, "%llu", &(player_boards[i]));
-        // Read opponent board
-        dummy_return = fgets(buf, 30, fp);
-        // Turn a string rep of U64 into a U64 rep and store in opponent_boards[i]
-        sscanf(buf, "%llu", &(opponent_boards[i]));
-        // Read 64 previous MC search results
-        MCvals[i] = (float *) calloc(64, sizeof(float));
-        for (int j = 0; j < 64; ++j)
-        {
-            dummy_return = fgets(buf, 30, fp);
-            MCvals[i][j] = atof(buf);
-        }    
-        // Read saved value
-        dummy_return = fgets(buf, 30, fp);
-        saved_values[i] = atof(buf);
-        // Read winner 
-        dummy_return = fgets(buf, 30, fp);
-        result[i] = atoi(buf);
-    }
-    // Close file
-    fclose(fp);
-}
+// void read_game_info(std::string game_path, int *n_moves, U64 *player_boards, U64 *opponent_boards, float **MCvals, int *result, float *saved_values)
+// {
+
+//     // dummy holder
+//     char* dummy_return;
+//     // Open file
+//     FILE *fp = fopen(game_path.c_str(),"r");
+//     // Read number of moves
+//     char *buf = (char *) calloc(30, sizeof(char));
+//     dummy_return = fgets(buf, 30, fp);
+//     *n_moves = atoi(buf);
+//     // Allocate memory
+//     player_boards = (U64 *) calloc(n_moves[0], sizeof(U64)); 
+//     opponent_boards = (U64 *) calloc(n_moves[0], sizeof(U64)); 
+//     MCvals = (float **) calloc(n_moves[0], sizeof(float *));
+//     result = (int *) calloc(n_moves[0], sizeof(int));
+//     saved_values = (float *) calloc(n_moves[0], sizeof(float));
+//     // Loop over number of moves
+//     for (int i = n_moves[0]-1;i >= 0; --i)
+//     {
+//         // Read player board
+//         dummy_return = fgets(buf,30,fp);
+//         // Turn a string rep of U64 into a U64 rep and store in player_boards[i]
+//         sscanf(buf, "%llu", &(player_boards[i]));
+//         // Read opponent board
+//         dummy_return = fgets(buf, 30, fp);
+//         // Turn a string rep of U64 into a U64 rep and store in opponent_boards[i]
+//         sscanf(buf, "%llu", &(opponent_boards[i]));
+//         // Read 64 previous MC search results
+//         MCvals[i] = (float *) calloc(64, sizeof(float));
+//         for (int j = 0; j < 64; ++j)
+//         {
+//             dummy_return = fgets(buf, 30, fp);
+//             MCvals[i][j] = atof(buf);
+//         }    
+//         // Read saved value
+//         dummy_return = fgets(buf, 30, fp);
+//         saved_values[i] = atof(buf);
+//         // Read winner 
+//         dummy_return = fgets(buf, 30, fp);
+//         result[i] = atoi(buf);
+//     }
+//     // Close file
+//     fclose(fp);
+// }
 
 /*  INPUT:
  *          model_path and local_rank are same as save_game_info
@@ -284,21 +299,21 @@ void Params::read_game_info(std::string game_path, int *n_moves, U64 *player_boa
  *          save the 4 values incase something takes a long time we can figure out what it is.
  */
 
-void Params::save_timer_info(std::string model_path, int local_rank, 
+void save_timer_info(std::string model_path, int local_rank, 
                                                  std::chrono::duration<double, std::nano> gt, 
                                                  std::chrono::duration<double, std::nano> w1t) 
 {
     FILE *fp;
-    char *fnm = (char *) calloc(50,sizeof(char));
-    sprintf(fnm,"%s/games/%d.timers",model_path.c_str(),local_rank);
-    fp = fopen(fnm,"w");
-    fprintf(fp,"%g  %g",gt.count(), w1t.count());
+    char *fnm = (char *) calloc(50, sizeof(char));
+    sprintf(fnm,"%s/games/%d.timers",model_path.c_str(), local_rank);
+    fp = fopen(fnm, "w");
+    fprintf(fp,"%g  %g", gt.count(), w1t.count());
     fclose(fp);
 }
 
 
 /* MRD I copied this from play.cpp but added the MC_chances parameter and the few lines that use it*/
-int Params::play_game(Engine* e, std::vector<MonteCarlo*> players, int* num_moves, float **MC_chances, int* total_MC_chances, 
+int play_game(Engine* e, std::vector<MonteCarlo*> players, int* num_moves, float **MC_chances, int* total_MC_chances, 
                     float *saved_values, int* no_decision_arr, int* decisions_made, bool print_on)
 {
     int move;
@@ -325,14 +340,12 @@ int Params::play_game(Engine* e, std::vector<MonteCarlo*> players, int* num_move
             MC_chances[num_moves[0]][i] = MCc[i]; 
             // printf("%f, ", MC_chances[num_moves[0]][i]);
         }
-        // printf("\n");
-        // exit(0);
+
         saved_values[num_moves[0]] = players[BLACK]->get_saved_value();
         no_decision_arr[num_moves[0]] = players[BLACK]->no_decision;
         decisions_made[0] += (1-players[BLACK]->no_decision);
         num_moves[0]++;
 
-        // exit(0);
         //TEMP
         // std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
         //TEMP
@@ -367,227 +380,7 @@ int Params::play_game(Engine* e, std::vector<MonteCarlo*> players, int* num_move
 }
 
 
-void Params::call_python_script_helper(new_params params, std::string model_name)
-{
-    std::string command = "python python_model_communicator.py " + params.semaphore_name + " " + 
-                            params.shared_memory_name + " " + 
-                            model_name;
-
-    printf("EXECUTING: %s\n", command.c_str());
-    pid_t pid = fork();
-    if(pid != 0)
-    {
-        int ret_val = system(command.c_str());
-        exit(0);
-    }
-}
-
-
-// model and python communication
-// 0 is success
-// 1 is semaphore error
-// 2 is shared memory error
-int Params::setup_python_communication()
-{
-    // printf("SEEDING RANDOM\n");
-    // srand (time(NULL));
-
-    pSemaphore = NULL;
-    pSharedMemory_code = NULL;
-    pSharedMemory_rest = NULL;
-
-    // sender flag
-    send_code = -1; // -1 is uninitilized, 0 is c sent, 1 is python sent, 2 is c sent python kill
-
-    
-    printf("COMMUNICATION CHANNEL INITIALIZING IN C++!\n");
-
-    // Create the shared memory
-    fd = shm_open(params.shared_memory_name.c_str(), O_RDWR | O_CREAT | O_EXCL, params.permissions);    
-
-    if (fd == -1) 
-    {
-        fd = 0;
-        printf("Creating the shared memory failed\n");
-        return 2;
-    }
-
-    else 
-    {
-        // The memory is created as a file that's 0 bytes long. Resize it.
-        rc = ftruncate(fd, params.size);
-        if (rc) 
-        {
-            printf("Resizing the shared memory failed\n");
-            return 2;
-        }
-        else 
-        {
-            // MMap the shared memory
-            //void *mmap(void *start, size_t length, int prot, int flags, int fd, off_t offset);
-            pSharedMemory_code = mmap((void *)0, (size_t)params.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-            if (pSharedMemory_code == MAP_FAILED) 
-            {
-                pSharedMemory_code = NULL;
-                printf("MMapping the shared memory failed\n");
-                return 2;
-            }
-            else 
-            {
-                pSharedMemory_rest = (void*) (((int32_t*) pSharedMemory_code) + 1);
-                printf("pSharedMemory_code = %p\n", pSharedMemory_code);
-                printf("pSharedMemory_rest = %p\n", pSharedMemory_rest);
-            }
-        }
-    }
-    
-    if (pSharedMemory_code) 
-    {
-        // Create the semaphore
-        pSemaphore = sem_open(params.semaphore_name.c_str(), O_CREAT, params.permissions, 0);
-    
-        if (pSemaphore == SEM_FAILED) 
-        {
-            printf("Creating the semaphore failed\n");
-            return 1;
-        }
-        else 
-        {
-            printf("pSemaphore =  %p\n", (void *)pSemaphore);
-        }
-    }
-
-    printf("communication channel established\n");
-    return 0;
-}
-
-void Params::send_end_code_python()
-{
-    send_code = 2;
-    memcpy(pSharedMemory_code, &send_code, sizeof(int32_t));
-}
-
-// 0 is success
-// 1 is semaphore error
-// 2 is shared memory error
-int Params::destroy_communication()
-{
-    send_end_code_python();
-
-    // Announce for one last time that the semaphore is free again so that python can quit
-    printf("Final release of the semaphore and send_code followed by a 5 second pause\n"); 
-    rc = release_semaphore(pSemaphore);
-    sleep(1); // race condition, where the python takes 1 seconds to quit
-
-    printf("Final wait to acquire the semaphore\n"); 
-    rc = acquire_semaphore(pSemaphore);
-    if (!rc) 
-    {
-        printf("Destroying the shared memory.\n");
-
-        rc = munmap(pSharedMemory_code, (size_t)params.size); // Un mmap the memory
-        if (rc) 
-        {
-            printf("Unmapping the memory failed\n");
-            return 2;
-        }
-        
-        if (-1 == close(fd)) // close file descriptor 
-        {
-            printf("Closing the memory's file descriptor failed\n");
-            return 2;
-        }
-    
-        rc = shm_unlink(params.shared_memory_name.c_str()); // destroy the shared memory.
-        if (rc) 
-        {
-            printf("Unlinking the memory failed\n");
-            return 2;
-        }
-    }
-
-    printf("Destroying the semaphore.\n");
-    // Clean up the semaphore
-    rc = sem_close(pSemaphore);
-    if (rc) 
-    {
-        printf("Closing the semaphore failed\n");
-        return 1;
-        
-    }
-    rc = sem_unlink(params.semaphore_name.c_str());
-    if (rc) 
-    {
-        printf("Unlinking the semaphore failed\n");
-        return 1;
-        
-    }
-
-    return 0;
-}
-
-void Params::cleanup()
-{
-    printf("begining communication cleanup\n");
-    int ret_val = destroy_communication();
-    if(ret_val == 1)
-    {
-        printf("Setup python communication returned with error 1 (problems with semaphore) exiting.");
-        exit(0);
-    }
-    else if(ret_val == 2)
-    {
-        printf("Setup python communication returned with error 2 (problems with shared memory) exiting.");
-        exit(0);
-    }
-    printf("finished communication cleanup\n");
-}
-
-
-// https://stackoverflow.com/questions/440133/how-do-i-create-a-random-alpha-numeric-string-in-c
-std::string Params::gen_random(const int len) 
-{
-    std::string new_str;
-    for(int i = 0; i < len; i++)
-    {
-        new_str.append(" ");
-    }
-
-    static const char alphanum[] =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-
-    for (int i = 0; i < len; ++i) 
-    {
-        new_str[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-    }
-    return new_str;
-}
-
-int Params::release_semaphore(sem_t *pSemaphore) 
-{
-    int rc = 0;
-    rc = sem_post(pSemaphore);
-    if(rc) 
-    {
-        printf("Releasing the semaphore failed\n");
-    }
-    return rc;
-}
-
-int Params::acquire_semaphore(sem_t *pSemaphore) 
-{
-    int rc = 0;
-    rc = sem_wait(pSemaphore);
-    if(rc) 
-    {
-        printf("Acquiring the semaphore failed\n");
-    }
-    return rc;
-}
-
-
-void Params::run_params(int local_rank, int games_per_proc, int iterations_per_move, bool print_on)
+void run_params(int local_rank, int games_per_proc, int iterations_per_move, bool print_on)
 {
     if (local_rank == -1)
     {
@@ -618,77 +411,12 @@ void Params::run_params(int local_rank, int games_per_proc, int iterations_per_m
  
 
 
-    // Make the two players for the next game
-    // I had to change this to MonteCarlo* instead of Player* 
-    // because if it was Player*, then when I want to call
-    // get_saved_action_probs() in the play_game function,
-    // the compiler complains
-
-    /*
-    param.cpp: In function ‘int play_game(Engine*, std::vector<Player*>, int*, float**)’:
-    param.cpp:216:31: error: ‘class Player’ has no member named ‘get_saved_action_probs’
-                     MCc = players[BLACK]->get_saved_action_probs();
-                                                                 ^
-    param.cpp:227:31: error: ‘class Player’ has no member named ‘get_saved_action_probs’
-                     MCc = players[WHITE]->get_saved_action_probs();
-                                                                 ^
-    make: *** [param] Error 1
-    */
-
-    // But changing it to MonteCarlo* works...
-    // Although if we want to use a minimax in part of the parameterization,
-    // it won't work as it is now.
-    // std::vector<Player*> players;
     std::vector<MonteCarlo*> players;
 
 
 
-
-    //////////////////////// CODE FOR MONTECARLO PYTHON COMMUNICATOR //////////////////////
-
-
-
-    // asigning values to struct
-    params.semaphore_name = gen_random(10);
-    params.shared_memory_name = gen_random(10);
-    params.size = 4096;
-    // params.semaphore_name = "other_stuff";
-    // params.shared_memory_name = "shared_memory_1";
-    params.permissions = 0600;
-
-    printf("params - size: %d\n", params.size);
-    printf("params - semaphore_name: %s\n", params.semaphore_name.c_str());
-    printf("params - shared_memory_name: %s\n", params.shared_memory_name.c_str());
-    printf("params - permissions: %d\n", params.permissions);
-
-    call_python_script_helper(params, model_name);
-
-
-
-    // inits
-
-
-
-
-
-    // spawn pythoner
-    int ret_val = setup_python_communication();
-    if(ret_val == 1)
-    {
-        printf("Setup python communication returned with error 1 (problems with semaphore) exiting.");
-        exit(0);
-    }
-    else if(ret_val == 2)
-    {
-        printf("Setup python communication returned with error 2 (problems with shared memory) exiting.");
-        exit(0);
-    }
-
-
-    ///////////////////////////////////////////////////////////////////////////////////////
-
-
-
+    PyCommunicator* comm = new PyCommunicator(model_name);
+    comm->setup_python_communication();
 
 
 
@@ -696,9 +424,9 @@ void Params::run_params(int local_rank, int games_per_proc, int iterations_per_m
     /* currently, there's an error when a MonteCarlo player tries to play */
     // black must be pushed onto the vector first
     players.push_back(new MonteCarlo(BLACK, e, model_name, iterations_per_move, true, 
-                        pSemaphore, pSharedMemory_code, pSharedMemory_rest)); // black
+                        comm)); // black
     players.push_back(new MonteCarlo(WHITE, e, model_name, iterations_per_move, true,
-                        pSemaphore, pSharedMemory_code, pSharedMemory_rest)); // white
+                        comm)); // white
      
     // Variable for the number of moves in the game
     int* num_moves = (int*) calloc(1, sizeof(int));
@@ -734,7 +462,6 @@ void Params::run_params(int local_rank, int games_per_proc, int iterations_per_m
         free(decisions_made);
         e->reset_engine();
     }
-    players.clear();
 
     if (print_on) std::cout << "Processor: " << local_rank << " finished playing and saving " << games_per_proc << " games." << std::endl;
         
@@ -757,21 +484,25 @@ void Params::run_params(int local_rank, int games_per_proc, int iterations_per_m
 
     if (print_on) printf("Cleaning up engine and players\n");
 
-    // clean up
+    /////////////// clean up ///////////////
+    
     players[BLACK]->cleanup();
     players[WHITE]->cleanup();
-
     delete(players[0]);
     delete(players[1]);
     players.clear();
     players.shrink_to_fit();
+
     e->clean_up();
     delete(e);
+
     free(num_moves);
+    comm->clean_up();
+    delete(comm);
 
-    cleanup();
+    ////////////////////////////////////////
 
-    if (print_on) printf("param_serial has finished\n");
+    if (print_on) printf("record_games has finished\n");
 }
 
 
@@ -794,10 +525,8 @@ int main(int argc, char * argv[])
     }
 
     srand(time(NULL)); // seed rand
-
     bool print_on = false;
-
-    Params* p = new Params();
-    p->run_params(local_rank, games_per_proc, iterations_per_move, print_on);
+    
+    run_params(local_rank, games_per_proc, iterations_per_move, print_on);
     return 0;
 }
